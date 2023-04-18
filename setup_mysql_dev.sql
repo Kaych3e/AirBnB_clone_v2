1be7909a2ce61fbd94ed4873e9eafe71acf19ff9
Kaych3e <nkaduba@gmail.com>
2023-04-18 10:20:18 +0000

    AIRBNB V2

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -2,10 +2,10 @@
 -- Create the database hbnb_dev_db
 CREATE DATATBASE IF NOT EXISTS hbnb_dev_db;
 -- Create a new user hbnb_dev (in localhost) with password hbnb_dev_pwd
-CREATE USER IF NOT EXISTS `hbnb_dev`@`localhost` IDENTIFIED BY `hbnb_dev_pwd`;
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 -- Grant all privileges on only the hbnb_dev_db database to hbnb_dev
-GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO `hbnb_dev`@`localhost`;
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev`@`localhost';
 FLUSH PRIVILEGES;
 -- Grant select privilegests to hbnb_dev on only performance_schema
-GRANT SELECT ON performance_schema.* TO `hbnb_dev`@`localhost`;
+GRANT SELECT ON performance_schema.* TO 'hbnb_dev`@`localhost';
 FLUSH PRIVILEGES;
