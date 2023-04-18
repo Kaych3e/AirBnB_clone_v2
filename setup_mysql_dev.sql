9ec5c88c5ec49ef9c08e5b0ed7f892f525877d44
Kaych3e <nkaduba@gmail.com>
2023-04-18 10:13:26 +0000

    AIRBNB V2

---
+++ setup_mysql_dev.sql
@@ -0,0 +1,12 @@
+-- Script thst prepares a MySQL server for the Airbnb clone project
+-- Create the database hbnb_dev_db
+CREATE DATATBASE IF NOT EXISTS hbnb_dev_db;
+
+-- Create a new user hbnb_dev (in localhost)
+CREATE USER IF NOT EXISTS `hbnb_dev`@`localhost` IDENTIFIED BY `hbnb_dev_pwd`;
+
+-- Grant all privileges on only the hbnb_dev_db database to hbnb_dev
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO `hbnb_dev`@`localhost`;
+
+-- Grant select privilegests to hbnb_dev on only performance_schema
+GRANT SELECT ON performance_schema.* TO `hbnb_dev`@`localhost`;
