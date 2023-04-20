5f86297cd56c8784a60c9460ea8cb82abc197abe
james-eo <108140375+james-eo@users.noreply.github.com>
2023-04-20 03:55:34 +0100

    MySQL server for the project

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,12 +1,13 @@
--- Script thst prepares a MySQL server for the Airbnb clone project
--- Create the database hbnb_dev_db
+-- This sript script that prepares a MySQL server for the project
+-- creating a database:hbnb_test_db
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
--- Create a new user hbnb_dev (in localhost) with password hbnb_dev_pwd
-CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost'
-IDENTIFIED BY 'hbnb_dev_pwd';
--- Grant select privileges to hbnb_dev on only performance_schema
+--creating user: 'hbnb_test_db'@'localhost' with password
+CREATE  USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+-- granting privileges to the user: hbnb_test_db
+GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'loicalhost';
+-- Ensuring that the changes take effect immediately with flush privileges.
+FLUSH PRIVILEGES
+--granting SELECT privilege for the user on the database performance_schema
 GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
-FLUSH PRIVILEGES;
--- Grant all privileges on only the hbnb_dev_db database to hbnb_dev
-GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
+-- ensuring that the changes take effect immediately with flush privileges.
 FLUSH PRIVILEGES;
