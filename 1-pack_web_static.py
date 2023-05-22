690dbb13e28414f5dec5c3cabdeee79f7d89bc23
root <nkaduba@gmail.com>
2023-05-22 15:28:17 -0700

    WS

---
+++ 1-pack_web_static.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+""" Generates a .tgz archive from the contents of web_static with Fabric"""
+
+import os.path
+from datetime import datetime
+from fabric.api import local
+from fabric.context_managers import cd
+
+def do_pack():
+    """ Create tar archive of web_static directory """
+    if os.path.isdir("versions") == False:
+        if local('mkdir -p versions').failed == True:
+            return None
+
+    t = datetime.utcnow()
+    f_name = f"versions/web_static_{t.year}{t.month}{t.day}{t.hour}{t.minute}{t.second}.tgz"
+
+    with cd("versions"):
+        if local("tar -cvzf {} web_static".format(f_name)).failed == True:
+            return None
+
+        return f_name
