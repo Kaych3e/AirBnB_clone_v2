52ee70e2eb416c45dce9ed65bc32f159927523e7
root <nkaduba@gmail.com>
2023-05-23 00:46:10 +0000

    WS

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """ Fabric script that distributes an archive to your web servers """
 import os
-from fabric.api import hosts, put, run
+from fabric.api import hosts, put, run, env
 
 env.hosts = ['54.236.12.12', '54.82.134.241']
 def do_deploy(archive_path):
@@ -15,16 +15,16 @@ def do_deploy(archive_path):
         f_name = archive_path.split('/')[-1].split('.')[0]
 
         """Uncompress the archive and create directory"""
-        f_path = f"/data/web_static/releases/{f_name}"
-        run(f"mkdir {f_path}")
-        run(f"tar -xzf /tmp/{archive_path.split('/')[-1]} -C {f_path}")
+        f_path = "/data/web_static/releases/{}".format(f_name)
+        run("mkdir {}".format(f_path))
+        run("tar -xzf /tmp/{archive_path.split('/')[-1]} -C {}".format(f_path))
         
         """delete archive and symbolic link"""
-        run(f"rm /tmp/{archive_path.split('/')[-1]}")
+        run("rm /tmp/{}".format(archive_path.split('/')[-1]))
         run("rm /data/web_static/current")
 
         """Create new symbollic link"""
-        run(f"ln -s {f_path} /data/web_static/current")
+        run("ln -s {} /data/web_static/current").format(f_path)
 
         return True
     except:
