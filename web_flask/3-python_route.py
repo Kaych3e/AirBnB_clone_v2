d949369f9d6b9223304570a2be6cc57f4155fe15
root <nkaduba@gmail.com>
2023-05-24 05:57:19 -0700

    WF

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+""" Script that starts a Flask web application """
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    """ defines hello page"""
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """ defines the hbnb route """
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def displayCtext(text):
+    """ defines the displayc text replacing underscore symbols with a space """
+    return "C {}".format(text.replace('_', ' '))
+
+
+@app.route('/python/', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def displayPytext(text="is cool"):
+    """ defines the python text, replace underscore symbols with a space """
+    return "Python {}".format(text.replace('_', ' '))
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
